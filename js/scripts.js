07ba1d5c67f109e00548fcf6b15358959495f8d3
Kristy Inouye <kristyki@hawaii.edu>
2016-03-03 09:51:09 -1000

    Window

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 var $navToggle = $('.nav-toggle');
+var $mainNavUl = $('.main-nav ul');
+var $window = $(window);
 
 $navToggle.on('click', function() {
-  console.log('Nav toggle clicked!');
+  $mainNavUl.toggle();
+  $navToggle.toggleClass('active');
 });
+
+$(window).on('resize', function() {
+  if ($(window.width() > 767) {
+  $mainNavUl.show();
+  $navToggle.addClass('active');
+}
+})
